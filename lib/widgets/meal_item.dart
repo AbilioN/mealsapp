60de5bc882a01e2394719c52f20887e5e3e4541b
AbilioN <netobalby@gmail.com>
2024-06-13 10:55:39 +0200

    item trait

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -8,6 +9,11 @@ class MealItem extends StatelessWidget {
     required this.meal,
   });
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +57,14 @@ class MealItem extends StatelessWidget {
                     height: 12,
                   ),
                   Row(
-                    children: [],
+                    children: [
+                      MealItemTrait(
+                        icon: Icons.schedule,
+                        label: '${meal.duration} min',
+                      ),
+                      const SizedBox(width: 12),
+                      MealItemTrait(icon: Icons.work, label: complexityText)
+                    ],
                   )
                 ]),
               ),
